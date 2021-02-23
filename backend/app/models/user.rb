f1d9a354476be7d7841ab9962e553a638cd81479
Soichiro Ota <libarta.official@gmail.com>
2021-02-23 14:01:12 +0900

    Make a basic User model (including secure passwords)

---
+++ backend/app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+    before_save { self.email = email.downcase }
+    validates :name,  presence: true, length: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                        format: { with: VALID_EMAIL_REGEX },
+                        uniqueness: { case_sensitive: false }
+    has_secure_password validations: false
+    validates :password, # 半角英数字を各一文字以上含む6-100文字の文字列
+    format: {
+        with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i,
+        message: 'は英数6文字以上で指定してください。'
+    },
+    allow_blank: true
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_03_063737) do
+ActiveRecord::Schema.define(version: 2021_02_23_045452) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
